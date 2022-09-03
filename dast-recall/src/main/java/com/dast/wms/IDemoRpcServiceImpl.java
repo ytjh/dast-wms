7cf36d12d0b06e2a333541600454311dfb3c5217
ytjh <413143906@qq.com>
2022-09-03 22:50:16 +0800

    初始化

---
+++ dast-recall/src/main/java/com/dast/wms/IDemoRpcServiceImpl.java
@@ -0,0 +1,20 @@
+package com.dast.wms;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import top.dast.ro.DastResult;
+
+/**
+ * @author yuanst
+ * @title: IDemoRpcServiceImpl
+ * @projectName participant-center-parent
+ * @description: TODO
+ * @date 2022/5/2511:22
+ */
+@DubboService(version = "1.0.0", protocol = {"dubbo"})
+public class IDemoRpcServiceImpl implements IDemoRpcService {
+
+    @Override
+    public DastResult<?> demo() {
+        return DastResult.success("ssssssss");
+    }
+}
